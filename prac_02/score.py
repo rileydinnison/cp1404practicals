049ab5e0d3966ef1ad8230f9697406510fc71034
rileydinnison <riley.dinnison@my.jcu.edu.au>
2025-09-23 13:54:46 +1000

    Refactor determine result into function and add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,23 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_result(score))
+    random_score = random.randint(0,100)
+    print(determine_result(random_score))
+
+def determine_result(score):
+    """Determine result based on score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    return "Bad"
+
+main()
