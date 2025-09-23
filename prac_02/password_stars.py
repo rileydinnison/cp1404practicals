e6175ef1d2c42db538fd0ac8a170d594bb3d5455
rileydinnison <riley.dinnison@my.jcu.edu.au>
2025-09-23 13:18:14 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,11 +1,25 @@
-
 MINIMUM_NUMBER_OF_LETTERS = 3
 
-password = input("Enter password: ")
 
-while len(password) < MINIMUM_NUMBER_OF_LETTERS:
-    print("Invalid")
+def main():
+    password = get_password()
+
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """Print asterisks based on length of password"""
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password():
+    """Get password and determine if length is long enough"""
     password = input("Enter password: ")
+    while len(password) < MINIMUM_NUMBER_OF_LETTERS:
+        print("Invalid")
+        password = input("Enter password: ")
+    return password
 
-for i in range(len(password)):
-    print("*", end="")
+
+main()
