609fba41d4a307968149f48449ef4b10bafa44b8
rileydinnison <riley.dinnison@my.jcu.edu.au>
2025-09-30 12:09:50 +1000

    Add answer comments and random number code

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+# 15, 5, 14
+# 5, 20
+print(random.randrange(3, 10, 2))  # line 2
+# 3, 5, 7
+# 3, 9
+print(random.uniform(2.5, 5.5))  # line 3
+# 5.378493871986157, 4.134735199085128
+# 2.5, 5.5
+
+print(random.randint(1,100))
