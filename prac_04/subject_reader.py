497f331c999741461bc3d6b75e36d15b7ef5e510
rileydinnison <riley.dinnison@my.jcu.edu.au>
2025-10-07 14:15:20 +1000

    Add display subjects functions and modify read subjects function to return a list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,29 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    """Read subjects and display subjects"""
+    subject_details = read_subjects(FILENAME)
+    display_subjects(subject_details)
 
 
-def load_data(filename=FILENAME):
+def read_subjects(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subjects = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subjects.append(parts)
     input_file.close()
+    return subjects
+
+
+def display_subjects(subjects):
+    """Display subjects"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
+
 
 
 main()
