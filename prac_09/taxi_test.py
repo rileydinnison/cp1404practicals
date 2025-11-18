ba4d7adc55737f135bc024c1bab2fa8cc76978f6
rileydinnison <riley.dinnison@my.jcu.edu.au>
2025-11-18 12:48:49 +1000

    Complete prac 9 Taxi Test exercise

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,10 @@
+from prac_09.taxi import Taxi
+
+
+my_taxi = Taxi("Prius 1", 100)
+Taxi.drive(my_taxi, 40)
+print(my_taxi)
+Taxi.start_fare(my_taxi)
+Taxi.drive(my_taxi, 100)
+print(my_taxi)
+print("Current Fare: ", Taxi.get_fare(my_taxi))
